4af696a23399fb6fbdf498b0fcf6ef6cac934cfe
Eugene Pshenichnyy <pshenichniy.eugene@gmail.com>
2020-11-21 22:31:42 +0300

    Add pyrmont support

--- eth2deposit/settings.py
+++ eth2deposit/settings.py
@@ -20,7 +20,8 @@ MedallaSetting = BaseChainSetting(GENESIS_FORK_VERSION=bytes.fromhex('00000001')
 SpadinaSetting = BaseChainSetting(GENESIS_FORK_VERSION=bytes.fromhex('00000002'))
 # Eth2 "dress rehearsal" testnet (spec v0.12.3)
 ZinkenSetting = BaseChainSetting(GENESIS_FORK_VERSION=bytes.fromhex('00000003'))
-
+# Eth2 pre-launch testnet (spec v1.0.0)
+PyrmontSetting = BaseChainSetting(GENESIS_FORK_VERSION=bytes.fromhex('00002009'))
 
 MAINNET = 'mainnet'
 WITTI = 'witti'
@@ -28,6 +29,7 @@ ALTONA = 'altona'
 MEDALLA = 'medalla'
 SPADINA = 'spadina'
 ZINKEN = 'zinken'
+PYRMONT = 'pyrmont'
 ALL_CHAINS: Dict[str, BaseChainSetting] = {
     MAINNET: MainnetSetting,
     WITTI: WittiSetting,
@@ -35,6 +37,7 @@ ALL_CHAINS: Dict[str, BaseChainSetting] = {
     MEDALLA: MedallaSetting,
     SPADINA: SpadinaSetting,
     ZINKEN: ZinkenSetting,
+    PYRMONT: PyrmontSetting,
 }
 
 
